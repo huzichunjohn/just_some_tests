2efda9ea411ccd60e6898db1aa807cd72f8284a5
johnhu <huzichunjohn@126.com>
2017-09-29 08:45:10 +0000

    test migrate.

--- blog/models.py
+++ blog/models.py
@@ -20,3 +20,12 @@ class FileUpload(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(User, to_field='id')
     datafile = models.FileField()
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    date = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    slug = models.SlugField(null=False, unique=True)
+
+    def __str__(self):
+        return self.title
